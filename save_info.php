e91c8502473f7129e399b1850e2b28f162715f02
smoothcriminal890 <nettanyonas@gmail.com>
2026-05-17 19:23:19 +0300

    Update save_info.php

--- save_info.php
+++ save_info.php
@@ -9,4 +9,14 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+    //Handling user input and saving to database
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $sql = "INSERT INTO user_info (name, email) VALUES ('$name', '$email')";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
 ?>
