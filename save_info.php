73e7d4d6db8bc020a4ee8d0fa3c6ef6b58c4a9a7
smoothcriminal890 <nettanyonas@gmail.com>
2026-05-17 19:15:52 +0300

    Update save_info.php

--- save_info.php
+++ save_info.php
@@ -1,3 +1,12 @@
 <?php
-
+// Database connection parameters
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "user_info";
+// Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
