7ffd8d75b8a592afd9fe74a3228d7365d9d5c65f
smoothcriminal890 <nettanyonas@gmail.com>
2026-05-18 07:30:39 +0300

    Update save_info.php

--- save_info.php
+++ save_info.php
@@ -18,7 +18,7 @@
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
     //After saving user info, send an email.
-        $to = "nettanyonas@gmail.com"; //My email address
+        $to = "nettanyonas@gmail.com"; //Email addresses to receive the notification
         $subject = "New Sign-up Information";
         $message = "A new user has signed up to the Fancy Surprise Planner website with the following information:\n\nName: $name\nEmail: $email\nPhone: $phone";
         mail($to, $subject, $message);
